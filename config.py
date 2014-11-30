88cac5e3b63925083867812dbcc35437b45b970c
adriel <AdrielVelazquez@gmail.com>
2014-11-30 00:20:24 -0500

    contains the dev config, connections.py (which only connects to the localhost server since the config is a dev config. The make file which intiates the server and runs the tests.

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+ENVIRONMENT = "dev"
+DEBUG = True
+
+HOST = "127.0.0.1"
+PORT = 5000
+COUCHDB_SERVER = ""
+CLIENT_DB = "clients-db"
+
+# let custom config override defaults
+if os.environ.get('CLIENT_CONFIG', False):
+    import imp
+    imp.load_source('app.customconfig', os.environ['CLIENT_CONFIG'])
+    from app.customconfig import *
